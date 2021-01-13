c5387f152bfeb3c42a6d0fa04b51c3fcbeee7f05
SANKET SARKAR <47353707+sscodes@users.noreply.github.com>
2021-01-14 02:12:10 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 100
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 10,
+        maxlength: 100
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        maxlength: 1024
+    }
+})
+
+
+const User = mongoose.model('User', userSchema);
+
+function validate(user) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(100).required(),
+        email: Joi.string().min(10).max(100).required().email(),
+        password: Joi.string().min(7).max(100).required()
+    })
+    return schema.validate(user);
+}
+
+module.exports.userSchema = userSchema;
+module.exports.User = User;
+module.exports.validate = validate;
