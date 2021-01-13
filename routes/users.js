59f95622d4865a6e67a2153a6f15ace76a71bc4a
SANKET SARKAR <47353707+sscodes@users.noreply.github.com>
2021-01-14 02:11:40 +0530

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const route = express.Router();
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const { User, validate } = require('../models/user');
+
+route.post('/', async (req, res) => {
+    const result = validate(req.body);
+    if (!result)
+        return res.status(400).send(result.error.details[0].message);
+
+    let userSearch = await User.findOne({ email: req.body.email });
+    if(userSearch)
+        return res.status(400).send('Email already registered...');
+    
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try
+    {
+        await user.save();
+
+        res.send(_.pick(user, ['_id', 'name', 'email']));
+    }
+    catch(err)
+    {
+        for(field in err.errors)
+        {
+            res.status(400).send(err.errors[field].message);
+        }
+    }
+});
+
+route.get('/', async (req, res) => {
+    const users = await User.find();
+
+    res.send(users);
+});
+
+module.exports = route;
