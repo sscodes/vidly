c7c0ae097bf160cf82c7307d0cd089dac1d75582
SANKET SARKAR <47353707+sscodes@users.noreply.github.com>
2021-01-16 15:13:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');     //requiring the express framework
 const app = express();
 
@@ -17,8 +19,10 @@ app.use(express.json());
 
 app.use('/api/genres', genres); //use genres module in place of /api/genres
 app.use('/api/customers', customers);  //use customers module in place of /api/customers
-app.use('/api/movies', movies);  //use customers module in place of /api/movies
-app.use('/api/rentals', rentals);  //use customers module in place of /api/movies
+app.use('/api/movies', movies);  //use movies module in place of /api/movies
+app.use('/api/rentals', rentals);  //use rentals module in place of /api/rentals
+app.use('/api/users', users);  //use users module in place of /api/users
+app.use('/api/auth', auth);  //use auth module in place of /api/auth
 
 
 //creating environment variable
